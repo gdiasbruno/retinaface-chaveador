6158c995599f253c544c256b9344defda14cd0d5
gdiasbruno <gdias.bruno@gmail.com>
2024-03-09 15:43:57 +0000

    Add setup scripts

---
+++ detect.sh
@@ -0,0 +1,4 @@
+python3 detect_from_file_wider.py \
+--trained_model='Resnet50_Final.pth' \
+--input_file='test_memory_path.txt' \
+--output_file='predictions.txt'
---
+++ setup.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+
+curl \
+-H "Authorization: Bearer ya29.a0Ad52N3_tjhchLlYlOONayA1uxFCUr_SAWccgLUHzrDOpg_wYYxO-og3Wevv_Gz5vsJ-3T7hpRc9pukSm4oNHLqqBVBMk6Il_bWHfJYyJrLkY4fRPPHUC73aom5G1X4RFNDHA5VOlxGROs_z4Lh1zXrsFvLkYYyA85HK5aCgYKAV8SARASFQHGX2MiZAuF5ukXix_QL3nfBK6iDA0171" \
+https://www.googleapis.com/drive/v3/files/1Xol4SUBhzZoPh0Q0eCqtUdhCbFvDmYeW?alt=media \
+-o Resnet50_Final.pth
+
+
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
